f0a6d986b75ed9e3bf834fb5138002733a7d2f52
Yurii <zumbala2013@gmail.com>
2023-11-07 00:59:37 +0200

    Add animations module

--- ang-memory-cards/src/app/app.module.ts
+++ ang-memory-cards/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { CardCellComponent } from './components/card-cell/card-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardCellComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
