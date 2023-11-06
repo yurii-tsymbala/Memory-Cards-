2083c70a3cb757cfac1de8f1bfa3de2c5d8f5a41
Yurii <zumbala2013@gmail.com>
2023-11-07 01:02:31 +0200

    Refactor board with card cell array

--- ang-memory-cards/src/app/app.component.ts
+++ ang-memory-cards/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from './model/Card';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'ang-memory-cards';
+export class AppComponent implements OnInit {
+  cardCells: Card[] = [];
+  columsCount: number = 3;
+
+  ngOnInit(): void {
+    this.cardCells = [
+      new Card('penguin'),
+      new Card('crab'),
+      new Card('jellyfish'),
+      new Card('chick'),
+      new Card('frog'),
+      new Card('chick'),
+      new Card('elephant'),
+      new Card('penguin'),
+      new Card('elephant'),
+      new Card('jellyfish'),
+      new Card('crab'),
+      new Card('frog')
+    ];
+  }
 }
