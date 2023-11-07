ef5c3a68f56a8778ac34fafb8fb122bc1844603f
Yurii <zumbala2013@gmail.com>
2023-11-08 00:58:11 +0200

    Configure card match/unmatch logic, game over check

--- ang-memory-cards/src/app/app.component.ts
+++ ang-memory-cards/src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { Card } from './model/Card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  cardCells: Card[] = [];
+export class AppComponent implements OnInit, OnChanges {
   columsCount: number = 3;
+  cardCells: Card[] = [];
+  firstFlippedIndex = -1;
+  secondFlippedIndex = -1;
 
   ngOnInit(): void {
     this.cardCells = [
@@ -22,7 +24,55 @@ export class AppComponent implements OnInit {
       new Card('elephant'),
       new Card('jellyfish'),
       new Card('crab'),
-      new Card('frog')
+      new Card('frog'),
     ];
+  }
+
+  ngOnChanges(): void {
+    console.log(this.cardCells);
+  }
+
+  onCardClick(index: number) {
+    if (this.firstFlippedIndex === -1) {
+      this.firstFlippedIndex = index;
+      return;
+    }
+    this.secondFlippedIndex = index;
+    
+    for (const card of this.cardCells) {
+      card.isEnabled = false;
+    }
+    console.log(JSON.parse(JSON.stringify(this.cardCells))); 
+    setTimeout(this.checkMatch, 900); 
+  
+  }
+
+  checkMatch = () =>  { 
+    const firstFlippedCard = this.cardCells[this.firstFlippedIndex];
+    const secondFlippedCard = this.cardCells[this.secondFlippedIndex];
+
+    if (firstFlippedCard.imgName === secondFlippedCard.imgName) {
+      console.log('Match!');
+      this.firstFlippedIndex = -1;
+      this.checkGameOver();
+    } else {
+      console.log('No Match!');
+      firstFlippedCard.isFlipped = false;
+      secondFlippedCard.isFlipped = false;
+      this.firstFlippedIndex = -1;
+    }
+
+    for (const card of this.cardCells) {
+      card.isEnabled = !card.isFlipped;
+     }
+  }
+
+  checkGameOver() {
+    const unflippedCards = this.cardCells.filter((card) => !card.isFlipped);
+    if (unflippedCards.length > 0) {
+      return;
+    } else {
+      console.log('Victory! Congrats =)');
+    }
   }
 }
