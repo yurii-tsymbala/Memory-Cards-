bb40fdd482a4b0aa9b309629deef5b5d427b4a91
Yurii <zumbala2013@gmail.com>
2023-11-11 02:59:12 +0200

    Refactor App component to become root

---
+++ ang-memory-cards/src/app/app.components.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LevelsComponent } from './components/levels/levels.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  imports: [LevelsComponent, RouterModule]
+})
+
+export class AppComponent {
+  title = "levels";
+}
