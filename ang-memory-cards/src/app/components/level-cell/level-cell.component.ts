43eeddddad3b996679b4983d86e14d4245ae3091
Yurii <zumbala2013@gmail.com>
2023-11-11 03:01:41 +0200

    Configure LevelCell component

---
+++ ang-memory-cards/src/app/components/level-cell/level-cell.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Level } from 'src/app/model/Level';
+
+@Component({
+  standalone: true,
+  selector: 'level-cell',
+  templateUrl: './level-cell.component.html',
+  imports: [CommonModule, RouterLink]
+})
+export class LevelCellComponent {
+  @Input() level!: Level;
+  @Output() launchGameLevel = new EventEmitter<number>();
+
+  launchGame() {    
+    this.launchGameLevel.emit(this.level.cardsAmount);
+  }
+}
