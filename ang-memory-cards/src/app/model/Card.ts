6d99e74aebcf9e8bd7d29a94c494ac20115f2427
Yurii <zumbala2013@gmail.com>
2023-11-14 21:36:54 +0200

    Inject LevelService for dependency injection

--- ang-memory-cards/src/app/model/Card.ts
+++
@@ -1,9 +0,0 @@
-export class Card {
-  imgName: string;
-  isFlipped = false;
-  isEnabled = true;
-
-  constructor(imgName: string) {
-    this.imgName = imgName;
-  }
-}
--- ang-memory-cards/src/main.ts
+++ ang-memory-cards/src/main.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes';
 import { AppComponent } from './app/app.components';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { LevelService } from './app/services/level.service';
 
 bootstrapApplication(AppComponent, {
     providers: [
       provideRouter(routeConfig),
-      provideAnimations()
+      provideAnimations(),
+      LevelService
     ]
   })
   .catch((err) => console.error(err)); 
