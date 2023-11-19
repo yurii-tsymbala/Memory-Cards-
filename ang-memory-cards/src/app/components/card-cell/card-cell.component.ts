b5c5be1e410b68dba0d548d6304137487d31780e
Yurii <zumbala2013@gmail.com>
2023-11-19 15:11:05 +0200

    Refactor card-cell with @Hosts

--- ang-memory-cards/src/app/components/card-cell/card-cell.component.ts
+++ ang-memory-cards/src/app/components/card-cell/card-cell.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule} from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output, HostBinding, HostListener } from '@angular/core';
 import { Card } from 'src/app/models/Card';
 
 @Component({
@@ -14,6 +14,12 @@ export class CardCellComponent {
   @Input() cardIndex!: number;
   @Output() flippedCardIndex = new EventEmitter<number>();
 
+  @HostBinding('class.input-disabled')
+  get inputDisabled(): boolean {
+    return !this.card.isEnabled;
+  }
+
+  @HostListener('click')
   toggleFlip() {
     this.card.isFlipped = !this.card.isFlipped;
     this.flippedCardIndex.emit(this.cardIndex);
