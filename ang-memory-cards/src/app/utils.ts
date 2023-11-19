f4f007a8041a2fa498e7ecf02ddf2b39d9292c2c
Yurii <zumbala2013@gmail.com>
2023-11-19 14:40:27 +0200

    Configure resize for elements like levels,cards

---
+++ ang-memory-cards/src/app/utils.ts
@@ -0,0 +1,17 @@
+export function getGridCellSize(
+    columnsCount: number,
+    rowsCount: number
+): string {
+    const sizeX = `${80 / columnsCount}vw`;
+    const sizeY = `${80 / rowsCount}vh`;
+
+    const widthFactor = (0.8 * window.innerWidth) / columnsCount;
+    const heightFactor = (0.8 * window.innerHeight) / rowsCount;
+    const maxCardSize = 160;
+
+    if (Math.min(widthFactor, heightFactor) > maxCardSize) {
+        return `${maxCardSize}px`;
+    } else {
+        return widthFactor < heightFactor ? sizeX : sizeY;
+    }
+}
