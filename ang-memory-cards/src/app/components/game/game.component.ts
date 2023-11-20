168f4b40476db88c1973f8df8da02460d3f568dc
Yurii <zumbala2013@gmail.com>
2023-11-20 19:42:08 +0200

    Polish UI in Game Component

--- ang-memory-cards/src/app/components/game/game.component.ts
+++ ang-memory-cards/src/app/components/game/game.component.ts
@@ -7,6 +7,7 @@ import { PopupComponent } from '../popup/popup.component';
 import { Level } from 'src/app/models/Level';
 import { LevelService } from 'src/app/services/level.service';
 import { CardService } from 'src/app/services/card.service';
+import { getGridCellSize } from 'src/app/utils';
 
 @Component({
   standalone: true,
@@ -19,7 +20,7 @@ export class GameComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   currentLevel!: Level;
   columsCount: number = 3;
-  cardSize: string = "10rem";
+  cardSize: string = '10rem';
   cardCells: Card[] = [];
 
   @ViewChild('popup', { static: false }) popup!: PopupComponent;
@@ -28,9 +29,11 @@ export class GameComponent {
     private levelService: LevelService,
     private cardService: CardService
   ) {
-    this.currentLevel = this.levelService.getLevel(Number(this.route.snapshot.params['id']));
+    this.currentLevel = this.levelService.getLevel(
+      Number(this.route.snapshot.params['id'])
+    );
     this.columsCount = this.currentLevel.col;
-    
+
     this.cardCells = this.cardService.generateCards(this.currentLevel);
     cardService.cardsUpdate = (cards) => {
       this.cardCells = cards;
@@ -41,20 +44,16 @@ export class GameComponent {
       this.popup.open();
     };
 
+    window.addEventListener('resize', this.updateCardSize);
     this.updateCardSize();
   }
 
-  updateCardSize(){
-    const cardsCount =  this.currentLevel.cardsAmount;
-
-    if(cardsCount<= 12){
-      this.cardSize  = "10rem";
-    }else if(cardsCount<= 30){
-      this.cardSize  = "8rem";
-    }else{
-      this.cardSize  = "7rem";
-    }
-  }
+  updateCardSize = () => {
+    this.cardSize = getGridCellSize(
+      this.currentLevel.col,
+      this.currentLevel.row
+    );
+  };
 
   nextLevel() {
     this.updateCardSize();
@@ -64,5 +63,9 @@ export class GameComponent {
 
   onCardClick(index: number) {
     this.cardService.onCardClick(index);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.updateCardSize);
   }
 }
